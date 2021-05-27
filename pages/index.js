98a1849f2b101f81b27cd181bd770fb880f2d6f7
rsosseh <ray_sosseh@brown.edu>
2021-05-27 00:51:07 -0400

    url pull check

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,26 @@ import PixelzBody from '../components/PixelzBody';
 import React from 'react';
 
 class Home extends React.Component{
-  static async getInitialProps(ctx) {
-    const res = await fetch('https://m4dv5tc2t8.execute-api.us-east-1.amazonaws.com/Prod/helloWorld')
-    const json = await res.json()
-    return { 
-            title: json.title, 
-            desc: json.desc,
-            sprite_1: json.sprite_1,
-            sprite_2: json.sprite_2,
-            sprite_3: json.sprite_3,
-            body_image: json.body_image
-          }
+  static defaultProps = {
+    "body_image": "/wolf-rida.png",
+    "title": "I like messing around with pixelz",
+    "sprite_2": "/npc_mel-copy.png",
+    "sprite_3": "/ice-man-trans.gif",
+    "sprite_1": "/mc-copy.png",
+    "desc": "when there's nothing better to do, i love spending time making little creations like this guy over here."
   }
+  // static async getInitialProps(ctx) {
+    // const res = await fetch('https://m4dv5tc2t8.execute-api.us-east-1.amazonaws.com/Prod/helloWorld')
+    // const json = await res.json()
+    // return { 
+    //         title: json.title, 
+    //         desc: json.desc,
+    //         sprite_1: json.sprite_1,
+    //         sprite_2: json.sprite_2,
+    //         sprite_3: json.sprite_3,
+    //         body_image: json.body_image
+    //       }
+  // }
   render(){
     const { title, desc, sprite_1, sprite_2, sprite_3, body_image } = this.props;
     return(
